3596a0453b3961602054b867d1d341d1f91005d0
Gerardo Aguayo <geraaguayoc@gmail.com>
2022-04-23 12:26:59 -0500

    Se puso el main en funciones

--- Main.py
+++ Main.py
@@ -1,21 +1,26 @@
 from Usuario import *
 from Libro import *
 
-print("Hola!, soy Venus y estoy aquí para ser tu compañera de lecturas!")
-print("Hagamos unas preguntas para conocernos mejor")
-print("----------------------------------------------------------------")
-nombreUsr = input("¿Cual es tu nombre?: ")
-generoFav = input("¿Cual es tu genero favorito?: ")
-estadoActual = input("¿Estas leyendo algo por ahora? Escribe si o no: ")
-estadoActual.lower()
-#Se determina es estado actual del usr (leyendo o no)
-if estadoActual == "si":
-    estadoActual = True
-elif estadoActual == "no":
-    estadoActual = False
-else:
-    print("Se ha introducido una opcion incorrecta")
+def crearUsuario():
+    print("Hola!, soy Venus y estoy aquí para ser tu compañera de lecturas!")
+    print("Hagamos unas preguntas para conocernos mejor")
+    print("----------------------------------------------------------------")
+    nombreUsr = input("¿Cual es tu nombre?: ")
+    generoFav = input("¿Cual es tu genero favorito?: ")
+    estadoActual = input("¿Estas leyendo algo por ahora? Escribe si o no: ")
+    estadoActual.lower()
+    #Se determina es estado actual del usr (leyendo o no)
+    if estadoActual == "si":
+        estadoActual = True
+    elif estadoActual == "no":
+        estadoActual = False
+    else:
+        print("Se ha introducido una opcion incorrecta, intentelo de nuevo")
+        crearUsuario()
 
-#Se crea el usuario con los datos
-nombreUsr = Usuario(nombreUsr,generoFav,estadoActual)
-nombreUsr.mostrarDatosUsr()
+    #Se crea el usuario con los datos
+    nombreUsr = Usuario(nombreUsr,generoFav,estadoActual)
+    nombreUsr.mostrarDatosUsr()
+
+
+
